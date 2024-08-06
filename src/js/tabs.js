04ea814a8f4b43a28519344d566fb99298947cce
zimablue-temp <glue_ratios_0y@icloud.com>
2024-08-07 01:12:13 +0500

    v2

---
+++ src/js/tabs.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+        const tabButtons = document.querySelectorAll('.tab-button');
+        const tabContents = document.querySelectorAll('.tab-content');
+
+        tabButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const target = button.getAttribute('data-tab');
+
+                tabButtons.forEach(btn => btn.classList.remove('active'));
+                tabContents.forEach(content => content.classList.remove('active'));
+
+                button.classList.add('active');
+                document.getElementById(target).classList.add('active');
+            });
+        });
+    });
